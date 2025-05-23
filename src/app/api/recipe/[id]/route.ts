13847f57443e7ef9c3150370520b547f788f685c
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2025-05-23 10:10:14 +0200

    added few errors

--- src/app/api/recipe/[id]/route.ts
+++ src/app/api/recipe/[id]/route.ts
@@ -3,17 +3,22 @@ import { apiService } from "@/services/api-service";
 export const GET = async (
   req: Request,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  { params }:any
+  { params }: any
 ) => {
   try {
     const { id } = await params;
-    
+
     if (!id) {
       return new Response("Missing recipe ID", { status: 400 });
     }
     const recipe = await apiService.getRecipe(id);
 
-    return new Response(JSON.stringify(recipe), { status: 200 });
+    if (recipe) {
+      return new Response(JSON.stringify(recipe), { status: 200 });
+    } else {
+      return new Response("Not found", { status: 404 });
+    }
+
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
     return new Response("Fail!", { status: 500 });
